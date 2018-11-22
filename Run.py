c8a22919a39b7f97908870def9d8f37c5d356084
RonalzedSombruxo <44907084+RonalzedSombruxo@users.noreply.github.com>
2018-11-21 21:17:43 -0300

    Delete Run.py

--- Run.py
+++
@@ -1,8 +0,0 @@
-import gi;gi.require_version('Gtk','3.0');from gi.repository import Gtk as gtk, GdkPixbuf, Gdk
-from MenuBar import*
-
-
-win = MenuB()
-win.connect("destroy", gtk.main_quit)
-win.show_all()
-gtk.main()
